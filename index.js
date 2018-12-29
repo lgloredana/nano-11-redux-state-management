ff198ccc69f7d0581aa962a76720ab8ab1280fb1
Loredana Lungu <loredana.lungu@tradeshift.com>
2018-12-29 15:38:15 +0200

    refactor: use constants

--- index.js
+++ index.js
@@ -33,24 +33,30 @@ function createStore (reducer) {
 
 //App Code
 
-function todos(state = [], action) {
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
+function todo(state = [], action) {
   switch (action.type) {
-      case 'ADD_TODO' :
+      case ADD_TODO :
           return state.concat([action.todo]);
-      case 'REMOVE_TODO' :
+      case REMOVE_TODO :
           return state.filter( todo => (todo.id !== action.id));
-      case 'TOGGLE_TODO' :
+      case TOGGLE_TODO :
           return map.state(todo => todo.id !== action.id ? todo : Object.assign({}, todo, {complete: !todo.complete}));
       default :
           return state;
   }
 }
 
-function goals(state = [], action){
+function goal(state = [], action){
     switch (action.type){
-        case 'ADD_GOAL' :
+        case ADD_GOAL :
             return state.concat([action.goal]);
-        case 'REMOVE_GOAL' :
+        case REMOVE_GOAL :
             return state.filter( goal => (goal.id !== action.id));
         default :
             return state;
@@ -81,7 +87,7 @@ const unsubscribe = store.subscribe(showChange);
 unsubscribe();
 
 store.dispatch({
-    type: "ADD_TODO",
+    type: ADD_TODO,
     todo: {
         id : 0,
         name: "Learn Redux",
